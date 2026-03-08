22d5f12b85c120dd3a60fb8a64209856e8b02035
Derman <dermanyoav@gmail.com>
2026-03-08 18:02:29 +0200

    Coding task 1

--- 1-basic-node/1-javascript/task1.js
+++ 1-basic-node/1-javascript/task1.js
@@ -1,1 +1,35 @@
-console.log("[+] Node task 1 init");
+const fs = require("fs/promises");
+
+function loadUsers() {
+    return fs.readFile("data.json", "utf8")
+        .then(res => JSON.parse(res))
+        .catch(err => {
+            console.error("Failed to read file:", err.message);
+            return null;
+        });
+}
+
+async function printUsersOver25() {
+    const users = await loadUsers();
+
+    if (users == null) return;
+
+    for (const user of users) {
+        if (user.age > 25) {
+            console.log(`Name: ${user.name} | Age: ${user.age}`)
+        }
+    }
+}
+
+// Main
+async function main() {
+    await loadUsers().then(users => {
+        console.log("[+] Javascript fun #1");
+     console.log(users);
+    });
+
+    console.log("[+] Javascript fun #2");
+    await printUsersOver25()
+}
+
+main();
